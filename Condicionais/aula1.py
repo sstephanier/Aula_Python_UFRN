6b094a454a80e4e7f84bb97f986c4acc6a3eb48a
Stephanie Rodrigues <117938105+sstephanier@users.noreply.github.com>
2023-09-11 22:25:30 +0000

    Primeira atividade

--- Condicionais/aula1.py
+++ Condicionais/aula1.py
@@ -18,3 +18,17 @@ if not a or c:
   print ("Tudo verdade - eu entro no IF")
 else:
   print ("Todas as sentenças falsas")
+
+#  Atividade 01  Faça um programa em Python que solicita a idade de uma pessoa e depois imprime na tela se ela é menor de idade, maior de idade ou idosa.
+# >18 maior de idade < 65 idoso
+
+idade = 12
+
+if (idade > 18 and idade < 65):
+  print("maior de idade")
+elif (idade > 65):
+  print("idoso")
+else :
+  print("menor de idade")
+
+
